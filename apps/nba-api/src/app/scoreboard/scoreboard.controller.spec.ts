288fb6a8e3f14dee7d1cebf07f1ed4475065225c
matiushariman <matius.hariman@gmail.com>
2022-12-05 21:58:36 +0700

    [nba-api] update ScoreboardService with types

--- apps/nba-api/src/app/scoreboard/scoreboard.controller.spec.ts
+++ apps/nba-api/src/app/scoreboard/scoreboard.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpModule } from '@nestjs/axios';
 
+import type { GetScoreboardRes } from '@nba-app/types-nba';
+
 import { ScoreboardController } from './scoreboard.controller';
 import { ScoreboardService } from './scoreboard.service';
 
@@ -17,7 +19,22 @@ describe('ScoreboardController', () => {
 
   describe('getData', () => {
     it('should return "Welcome to nba-api!"', async () => {
-      const result = ['abc'];
+      const result: GetScoreboardRes = {
+        meta: {
+          version: 1,
+          request:
+            'https://nba-prod-us-east-1-mediaops-stats.s3.amazonaws.com/NBA/liveData/scoreboard/todaysScoreboard_00.json',
+          time: '2022-12-05 07:57:39.5739',
+          code: 200,
+        },
+        scoreboard: {
+          gameDate: '2022-12-04',
+          leagueId: '00',
+          leagueName: 'National Basketball Association',
+          games: [],
+        },
+      };
+
       jest
         .spyOn(scoreboard.get<ScoreboardService>(ScoreboardService), 'getData')
         .mockImplementation(async () => result);
--- apps/nba-api/src/app/scoreboard/scoreboard.service.ts
+++ apps/nba-api/src/app/scoreboard/scoreboard.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { catchError, firstValueFrom } from 'rxjs';
 
+import type { GetScoreboardRes } from '@nba-app/types-nba';
+
 @Injectable()
 export class ScoreboardService {
   constructor(private readonly httpService: HttpService) {}
 
   // TODO: improve typings
-  async getData(): Promise<string[]> {
+  async getData(): Promise<GetScoreboardRes> {
     const { data } = await firstValueFrom(
       this.httpService
-        .get<string[]>(
+        .get<GetScoreboardRes>(
           'https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json'
         )
         .pipe(
