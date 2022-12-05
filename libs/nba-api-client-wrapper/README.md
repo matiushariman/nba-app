2a98431032bd9de9559edc38f2476689e2894a81
matiushariman <matius.hariman@gmail.com>
2022-12-05 22:14:46 +0700

    [nba-api-client-wrapper] initialize nba-api-client-wrapper lib

---
+++ libs/nba-api-client-wrapper/README.md
@@ -0,0 +1,11 @@
+# nba-api-client-wrapper
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test nba-api-client-wrapper` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint nba-api-client-wrapper` to execute the lint via [ESLint](https://eslint.org/).
---
+++ libs/nba-api-client-wrapper/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'nba-api-client-wrapper',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/nba-api-client-wrapper',
+};
