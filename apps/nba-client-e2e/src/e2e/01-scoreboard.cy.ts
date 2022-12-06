7ed40f19fa96e7ebab7bf28dc5f24c569edb5a4a
matiushariman <matius.hariman@gmail.com>
2022-12-06 18:46:05 +0700

    [nba-client-e2e] replace cypress with playwright

--- apps/nba-client-e2e/src/e2e/01-scoreboard.cy.ts
+++
@@ -1,9 +0,0 @@
-import { getScoreboard } from '../support/scoreboard.po';
-
-describe('01-scoreboard', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should be able to see list of games', () => {
-    getScoreboard().should('be.visible');
-  });
-});
