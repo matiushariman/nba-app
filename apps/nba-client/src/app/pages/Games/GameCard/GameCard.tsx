57a74b02c3a85a2197c38aa8edcf7df0b04ef643
matiushariman <matius.hariman@gmail.com>
2022-12-07 13:53:38 +0700

    [nba-client] update mobile layout

--- apps/nba-client/src/app/pages/Games/GameCard/GameCard.tsx
+++ apps/nba-client/src/app/pages/Games/GameCard/GameCard.tsx
@@ -9,15 +9,15 @@ const GameCard = ({
   gameStatusText,
   gameLeaders,
 }: GameCardProps) => (
-  <div className="flex shadow-md cursor-pointer items-center bg-white rounded divide-x items-center">
-    <div className="w-[60%]">
+  <div className="flex shadow-md cursor-pointer items-center bg-white rounded md:divide-x items-center">
+    <div className="flex-grow md:w-[60%] md:flex-grow-0">
       <Scoreboard
         homeTeam={homeTeam}
         awayTeam={awayTeam}
         gameStatusText={gameStatusText}
       />
     </div>
-    <div className="flex-grow">
+    <div className="md:flex-grow hidden md:block">
       <GameLeaders gameLeaders={gameLeaders} />
     </div>
   </div>
