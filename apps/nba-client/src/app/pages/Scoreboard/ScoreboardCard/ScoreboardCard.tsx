87318eaed1dae3c0a19a0bef51692c19afa35d1e
matiushariman <matius.hariman@gmail.com>
2022-12-07 11:46:17 +0700

    [nba-client] update layout background color

--- apps/nba-client/src/app/pages/Scoreboard/ScoreboardCard/ScoreboardCard.tsx
+++ apps/nba-client/src/app/pages/Scoreboard/ScoreboardCard/ScoreboardCard.tsx
@@ -9,7 +9,7 @@ const ScoreboardCard = ({
   awayTeam,
   gameStatusText,
 }: ScoreboardCardProps) => (
-  <div className="pt-2 pb-2 pl-4 pr-4 flex gap-2 shadow-md cursor-pointer items-center">
+  <div className="pt-2 pb-2 pl-4 pr-4 flex gap-2 shadow-md cursor-pointer items-center bg-white rounded">
     <div className="flex gap-3 items-center">
       <ScoreboardTeamDetails
         teamId={awayTeam.teamId}
