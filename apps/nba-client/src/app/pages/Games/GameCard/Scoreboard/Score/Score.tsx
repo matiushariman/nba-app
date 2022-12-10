579aa54cbfe280bb7757283ca237cfba7e2ceed3
matiushariman <matius.hariman@gmail.com>
2022-12-10 20:50:06 +0700

    [nba-client] add ability to toggle score

--- apps/nba-client/src/app/pages/Games/GameCard/Scoreboard/Score/Score.tsx
+++ apps/nba-client/src/app/pages/Games/GameCard/Scoreboard/Score/Score.tsx
@@ -1,14 +1,16 @@
 import { useGameStatusContext } from '../../../../../context/GameStatusContext';
+import { useGameContext } from '../../../../../context/GameContext';
 
 import type { ScoreProps } from './Score.types';
 
 const Score = ({ score }: ScoreProps) => {
   const { hasGameStarted } = useGameStatusContext();
+  const { isScoreHidden } = useGameContext();
 
   return (
     <div className="w-[52px]">
       <p className="font-bold text-3xl text-center">
-        {hasGameStarted ? score : '-'}
+        {hasGameStarted && !isScoreHidden ? score : '-'}
       </p>
     </div>
   );
--- apps/nba-client/src/app/pages/Games/GameCard/Scoreboard/Score/__tests__/Score.test.tsx
+++ apps/nba-client/src/app/pages/Games/GameCard/Scoreboard/Score/__tests__/Score.test.tsx
@@ -2,6 +2,20 @@ import Score from '..';
 
 import { render, screen } from '../../../../../../utils/testUtils';
 
+jest.mock('../../../../../context/GameContext', () => ({
+  useGameContext: jest
+    .fn()
+    .mockReturnValueOnce({
+      isScoreHidden: false,
+    })
+    .mockReturnValueOnce({
+      isScoreHidden: false,
+    })
+    .mockReturnValueOnce({
+      isScoreHidden: true,
+    }),
+}));
+
 jest.mock('../../../../../context/GameStatusContext', () => ({
   useGameStatusContext: jest
     .fn()
@@ -10,6 +24,9 @@ jest.mock('../../../../../context/GameStatusContext', () => ({
     })
     .mockReturnValueOnce({
       hasGameStarted: false,
+    })
+    .mockReturnValueOnce({
+      hasGameStarted: true,
     }),
 }));
 
@@ -18,11 +35,15 @@ describe('pages/games/GameCard/Scoreboard/GameCard/Score', () => {
     render(<Score score={100} />);
   });
 
-  it('should render score if game has started', () => {
+  it('should render score if game has started & !isScoreHidden', () => {
     expect(screen.getByText(/100/i)).toBeVisible();
   });
 
   it('should render - if game has not started', () => {
+    expect(screen.getByText('-')).toBeVisible();
+  });
+
+  it('should render - if game has not started & isScoreHidden', () => {
     expect(screen.getByText('-')).toBeVisible();
   });
 });
