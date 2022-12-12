cfcb60dfdde1c76c34b728a8f5dba0df4bb996df
matiushariman <matius.hariman@gmail.com>
2022-12-12 19:39:29 +0700

    [nba-client-e2e] update e2e tests to use fixture

--- apps/nba-client-e2e/src/01-games.spec.ts
+++ apps/nba-client-e2e/src/01-games.spec.ts
@@ -1,21 +1,27 @@
-import { test } from '@playwright/test';
+import { test as base } from '@playwright/test';
 
 import { GamesSteps } from './steps/games-steps';
 
-test.describe('01-games', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  test('User should be able to see list of today`s games', async ({ page }) => {
+// Extend basic test by providing a "gamesSteps" fixture.
+const test = base.extend<{ gamesSteps: GamesSteps }>({
+  gamesSteps: async ({ page }, use) => {
     const gamesSteps = new GamesSteps(page);
+
+    await page.goto('/');
+
+    await use(gamesSteps);
+  },
+});
+
+test.describe('01-games', () => {
+  test('User should be able to see list of today`s games', async ({
+    gamesSteps,
+  }) => {
     await test.step('Verify that list of games is displayed', async () =>
       gamesSteps.isListOfTodaysGamesVisible());
   });
 
-  test('User should be able to hide score', async ({ page }) => {
-    const gamesSteps = new GamesSteps(page);
-
+  test('User should be able to hide score', async ({ gamesSteps }) => {
     await test.step('User press hide score toggle', async () =>
       gamesSteps.toggleHideScore());
 
--- apps/nba-client-e2e/src/02-game-details.spec.ts
+++ apps/nba-client-e2e/src/02-game-details.spec.ts
@@ -1,17 +1,22 @@
-import { test } from '@playwright/test';
+import { test as base } from '@playwright/test';
 
 import { GameDetailsSteps } from './steps/game-details-steps';
 
-test.describe('02-game-details', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  test('User should be able to navigate to game details page', async ({
-    page,
-  }) => {
+// Extend basic test by providing a "gameDetailsSteps" fixture.
+const test = base.extend<{ gameDetailsSteps: GameDetailsSteps }>({
+  gameDetailsSteps: async ({ page }, use) => {
     const gameDetailsSteps = new GameDetailsSteps(page);
 
+    await page.goto('/');
+
+    await use(gameDetailsSteps);
+  },
+});
+
+test.describe('02-game-details', () => {
+  test('User should be able to navigate to game details page', async ({
+    gameDetailsSteps,
+  }) => {
     await test.step('User clicks on one of the games', async () =>
       gameDetailsSteps.navigateToGameDetailsPage());
 
