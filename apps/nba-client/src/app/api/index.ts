9053c086dbbefffbc4979b9d6430852eed4a03eb
matiushariman <matius.hariman@gmail.com>
2022-12-16 23:26:59 +0800

    [nba-client] add NX_API_BASE_URL

--- apps/nba-client/src/app/api/index.ts
+++ apps/nba-client/src/app/api/index.ts
@@ -1,5 +1,7 @@
 import NbaApi from '@nba-app/nba-api-client-wrapper';
 
-const nbaApi = new NbaApi('http://localhost:3333');
+const nbaApi = new NbaApi(
+  process.env['NX_API_BASE_URL'] || 'http://localhost:3333'
+);
 
 export default nbaApi;
