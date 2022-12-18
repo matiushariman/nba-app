888ba124dc6cb1668d193722a9034674a1a2bebc
matiushariman <matius.hariman@gmail.com>
2022-12-18 16:36:44 +0700

    [nba-client] add tab

--- apps/nba-client/src/app/pages/GameDetails/GameDetails.tsx
+++ apps/nba-client/src/app/pages/GameDetails/GameDetails.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { Tab } from '@headlessui/react';
 
 import GameInfo from './GameInfo';
 import useGetGame from '../../hooks/useGetGame';
@@ -6,6 +7,8 @@ import useGetBoxscore from '../../hooks/useGetBoxscore';
 import GameScore from './GameScore';
 import Summary from './Summary';
 import LoadingText from '../../components/LoadingText';
+
+const TABS = ['Game Summary', 'Box Score'];
 
 const GameDetails = () => {
   const { gameId = '' } = useParams<{ gameId: string }>();
@@ -20,7 +23,30 @@ const GameDetails = () => {
     <>
       <GameScore />
       <GameInfo />
-      <Summary />
+      <Tab.Group>
+        <div className="bg-white">
+          <Tab.List className="container mx-auto">
+            {TABS.map((tab) => (
+              <Tab
+                key={tab}
+                className={({ selected }) =>
+                  `${
+                    selected && 'font-bold border-b-4 border-black'
+                  } pt-2 pb-2 pl-5 pr-5`
+                }
+              >
+                {tab}
+              </Tab>
+            ))}
+          </Tab.List>
+        </div>
+        <Tab.Panels>
+          <Tab.Panel>
+            <Summary />
+          </Tab.Panel>
+          <Tab.Panel>Content 2</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </>
   );
 };
