e5fc1c0d218fd0269b8cc071bc1886aa52ae659c
matiushariman <matius.hariman@gmail.com>
2022-12-19 17:04:13 +0700

    [api-client] remove top performers from game-details

--- apps/nba-client/src/app/pages/GameDetails/GameDetails.tsx
+++ apps/nba-client/src/app/pages/GameDetails/GameDetails.tsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 
 import GameInfo from './GameInfo';
-import useGetGame from '../../hooks/useGetGame';
 import useGetBoxscore from '../../hooks/useGetBoxscore';
 import GameScore from './GameScore';
 import Summary from './Summary';
@@ -9,10 +8,9 @@ import LoadingText from '../../components/LoadingText';
 
 const GameDetails = () => {
   const { gameId = '' } = useParams<{ gameId: string }>();
-  const { isLoading: isLoadingGame } = useGetGame({ gameId });
   const { isLoading: isLoadingBoxscore } = useGetBoxscore({ gameId });
 
-  if (isLoadingGame || isLoadingBoxscore) {
+  if (isLoadingBoxscore) {
     return <LoadingText />;
   }
 
--- apps/nba-client/src/app/pages/GameDetails/Summary/Linescores/Linescores.tsx
+++ apps/nba-client/src/app/pages/GameDetails/Summary/Linescores/Linescores.tsx
@@ -1,5 +1,4 @@
 import LinescoresTable from './LinescoresTable';
-import TopPerformers from './TopPerformers';
 import { Card } from '../../../../components/ui';
 
 const Linescores = () => (
@@ -11,16 +10,6 @@ const Linescores = () => (
         </div>
         <div className="pt-5">
           <LinescoresTable />
-        </div>
-      </div>
-    </div>
-    <div className="md:col-span-4">
-      <div className="grid gap-3 divide-y pt-6 md:pl-6 md:pt-0">
-        <div>
-          <p className="text-2xl font-bold uppercase">Top Performers</p>
-        </div>
-        <div className="pt-5">
-          <TopPerformers />
         </div>
       </div>
     </div>
