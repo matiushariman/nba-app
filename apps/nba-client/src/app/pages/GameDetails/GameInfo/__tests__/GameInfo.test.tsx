7ac4bb692ebe95e2da27ddb64b0062cd80233d96
matiushariman <matius.hariman@gmail.com>
2022-12-19 17:01:57 +0700

    [api-client] update selectors to use boxscore

--- apps/nba-client/src/app/pages/GameDetails/GameInfo/__tests__/GameInfo.test.tsx
+++ apps/nba-client/src/app/pages/GameDetails/GameInfo/__tests__/GameInfo.test.tsx
@@ -1,19 +1,6 @@
 import GameInfo from '..';
 import { render } from '../../../../utils/testUtils';
 
-jest.mock('../../../hooks/useGetGame', () => ({
-  selectGame: jest.fn().mockReturnValueOnce({
-    awayTeam: {
-      teamId: '1',
-      teamName: 'Lakers',
-    },
-    homeTeam: {
-      teamId: '2',
-      teamName: 'Celtics',
-    },
-  }),
-}));
-
 jest.mock('../../../hooks/useGetBoxscore', () => ({
   selectArena: jest.fn().mockReturnValueOnce({
     arenaName: 'TD Garden',
@@ -21,6 +8,19 @@ jest.mock('../../../hooks/useGetBoxscore', () => ({
     arenaState: 'MA',
   }),
   selectGameTime: jest.fn().mockReturnValueOnce('2022-12-10T22:00:00Z'),
+  selectGameScore: jest.fn().mockReturnValueOnce({
+    awayTeam: {
+      teamId: '1',
+      teamName: 'Lakers',
+      score: '92',
+    },
+    homeTeam: {
+      teamId: '2',
+      teamName: 'Celtics',
+      score: '131',
+    },
+    gameStatusText: 'FINAL',
+  }),
 }));
 
 describe('pages/GameDetails/GameInfo', () => {
